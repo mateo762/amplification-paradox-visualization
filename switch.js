06d67eaf44a7d487f61e6a4b1978608567b927f3
Mateo Echeverry <mateo.echeverryhoyos@gmail.com>
2023-03-18 17:12:51 +0100

    Remove switch.js

--- switch.js
+++
@@ -1,8 +0,0 @@
-// Add an event listener to the checkbox
-document.querySelector("#toggle").addEventListener("change", updateToggleLabel);
-
-// Function to update the label based on the checkbox's checked state
-function updateToggleLabel() {
-  const isChecked = document.querySelector("#toggle").checked;
-  document.querySelector("#toggle-label").textContent = isChecked ? "Utility" : "Random";
-}
