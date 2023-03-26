027d08c266931b78b1c6fa2c81af3b433707fcdb
Mateo Echeverry <mateo.echeverryhoyos@gmail.com>
2023-03-26 20:33:31 +0200

    Change plots.html

--- plots/plots.js
+++ plots/plots.js
@@ -1,13 +1,45 @@
 const plotNames = ['Start:C/Measure:L', 'Start:CR/Measure:L', 'Start:R/Measure:L',
-       'User:C/Measure:L', 'User:CR/Measure:L', 'User:R/Measure:L',
-       'Start:C/Measure:CL', 'Start:CR/Measure:CL', 'Start:R/Measure:CL',
-       'User:C/Measure:CL', 'User:CR/Measure:CL', 'User:R/Measure:CL',
-       'Start:C/Measure:C', 'Start:CR/Measure:C', 'Start:R/Measure:C',
-       'User:C/Measure:C', 'User:CR/Measure:C', 'User:R/Measure:C',
-       'Start:C/Measure:CR', 'Start:CR/Measure:CR', 'Start:R/Measure:CR',
-       'User:C/Measure:CR', 'User:CR/Measure:CR', 'User:R/Measure:CR',
-       'Start:C/Measure:R', 'Start:CR/Measure:R', 'Start:R/Measure:R',
-       'User:C/Measure:R', 'User:CR/Measure:R', 'User:R/Measure:R']
+    'User:C/Measure:L', 'User:CR/Measure:L', 'User:R/Measure:L',
+    'Start:C/Measure:CL', 'Start:CR/Measure:CL', 'Start:R/Measure:CL',
+    'User:C/Measure:CL', 'User:CR/Measure:CL', 'User:R/Measure:CL',
+    'Start:C/Measure:C', 'Start:CR/Measure:C', 'Start:R/Measure:C',
+    'User:C/Measure:C', 'User:CR/Measure:C', 'User:R/Measure:C',
+    'Start:C/Measure:CR', 'Start:CR/Measure:CR', 'Start:R/Measure:CR',
+    'User:C/Measure:CR', 'User:CR/Measure:CR', 'User:R/Measure:CR',
+    'Start:C/Measure:R', 'Start:CR/Measure:R', 'Start:R/Measure:R',
+    'User:C/Measure:R', 'User:CR/Measure:R', 'User:R/Measure:R']
+
+function createAxis(id) {
+    // Set up the SVG element
+    const margin = { top: 10, right: 10, bottom: 20, left: 50 };
+    const width = 200 - margin.left - margin.right;
+    const height = 150 - margin.top - margin.bottom;
+
+    // Define the scales
+    const x = d3.scaleLinear().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
+
+    // Create the axes
+    const xAxis = d3.axisBottom(x).ticks(0)
+    const yAxis = d3.axisLeft(y).ticks(0)
+
+    const svg = d3
+        .select(`#${id}`)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("class", `svg-${id}`)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`)
+
+    svg
+        .append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(xAxis);
+
+    // Add the y-axis
+    svg.append("g").call(yAxis);
+}
 
 function createPlot(id, plotName, data) {
     // Set up the SVG element
@@ -21,7 +53,7 @@ function createPlot(id, plotName, data) {
 
     // Create the axes
     const xAxis = d3.axisBottom(x).ticks(11);
-    const yAxis = d3.axisLeft(y).tickFormat(d => (d*100).toFixed(2)).ticks(5);
+    const yAxis = d3.axisLeft(y).tickFormat(d => (d * 100).toFixed(2)).ticks(5);
 
     // Define the line generator
     const line = d3
@@ -30,8 +62,7 @@ function createPlot(id, plotName, data) {
         .y((d) => y(d.Value));
 
     const svg = d3
-        .select(`#${id}`)
-        .append("svg")
+        .select(`.svg-${id}`)
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -62,10 +93,13 @@ function createPlot(id, plotName, data) {
     svg
         .append("g")
         .attr("transform", `translate(0,${height})`)
+        .transition()
         .call(xAxis);
 
     // Add the y-axis
-    svg.append("g").call(yAxis);
+    svg.append("g")
+        .transition()
+        .call(yAxis);
     // Draw the recommendation line with animation
     drawLineWithAnimation(recommendationData, "steelblue", svg, line);
 
@@ -97,9 +131,29 @@ function drawLineWithAnimation(data, color, svg, line) {
         .attr("stroke-dashoffset", 0);
 }
 
+let rightPlots = ['Start:C/Measure:R', 'Start:CR/Measure:R', 'Start:R/Measure:R',
+    'User:C/Measure:R', 'User:CR/Measure:R', 'User:R/Measure:R']
+
 d3.csv("https://mateo762.github.io/data/plots.csv.txt").then((data) => {
     let iteration = 1
-    for(let plotName of plotNames){
-        createPlot(`plot-${iteration++}`, plotName, data)
+    for (let plotName of plotNames) {
+        createAxis(`plot-${iteration++}`)
+    }
+    iteration = 1
+    for (let rightPlot of rightPlots) {
+        createAxis(`plot-compare-${iteration++}`)
+    }
+    iteration = 1
+    document.querySelector(".start-button").addEventListener("click", createPlots)
+
+    function createPlots() {
+        for (let plotName of plotNames) {
+            createPlot(`plot-${iteration++}`, plotName, data)
+        }
+        iteration = 1
+    for (let rightPlot of rightPlots) {
+        createPlot(`plot-compare-${iteration++}`, rightPlot, data)
+    }
     }
 });
+
