3f8552081392ff965bf151bd262fbf5481d30536
Mateo Echeverry <mateo.echeverryhoyos@gmail.com>
2023-05-09 16:23:42 +0200

    Refactor

--- data.js
+++
@@ -1,6 +0,0 @@
-// d3.csv("/data/employees.csv", function(data) {
-//     for (var i = 0; i < data.length; i++) {
-//         console.log(data[i].Name);
-//         console.log(data[i].Age);
-//     }
-// });
--- main.js
+++
@@ -1,5 +0,0 @@
-import { startPart1  } from "./script.js";
-import { startPart1B } from "./bar.js";
-
-startPart1()
-startPart1B()
